7d7a26c15d0058196b5378e09ac07f6f96a90a23
Denish <denishbhuva01@gmail.com>
2025-10-22 20:29:41 +0530

    base layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='bg-[#55d082] text-white rounded-full w-full h-[55px] flex justify-center items-center border-black border-2 cursor-pointer hover:bg-white hover:text-[#55d082] transition-all duration-300 font-bold'>
+        Ghostly
+      </div>
+    </>
+  )
+}
+
+export default Navbar
